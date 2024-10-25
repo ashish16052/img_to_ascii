c7ada062d89f1b9cac9cfb4abee8e947d4e7219f
ashish16052 <ashishmohite16052@gmail.com>
2024-10-25 23:28:00 +0530

    feat: convert bytes to pixel matrix

--- main.py
+++ main.py
@@ -6,5 +6,17 @@ img_data = list(img.getdata())
 
 
 # 2. bytes > pixel grid
+# [
+#     [(r,g,b), (r,g,b), (r,g,b)],
+#     [(r,g,b), (r,g,b), (r,g,b)],
+#     [(r,g,b), (r,g,b), (r,g,b)],
+#     [(r,g,b), (r,g,b), (r,g,b)],
+#     [(r,g,b), (r,g,b), (r,g,b)],
+# ]
+width, height = img.size
+pixels = [
+    img_data[i * width : (i + 1) * width ]
+    for i in range(0, height)
+]
 
 # 3. pixel grid -> ascii grid
